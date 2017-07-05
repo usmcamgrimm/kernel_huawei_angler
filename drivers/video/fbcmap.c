fa2cc784e2e8c17224ef1f871e88aa996fee14ad
Nathan Chancellor <natechancellor@gmail.com>
2017-07-04 23:54:41 -0700

    Revert some problematic backports for clean fast forward
    Revert "FROMLIST: 9p: fix a potential acl leak"
    This reverts commit aedf77d56472d1fddf050c61c1017d4f51149fb1.
    
    Revert "ANDROID: fix acl leaks"
    This reverts commit c4313d200dfdd8d8806ac256e6bc2fbd5d5dc162.
    
    Revert "BACKPORT: posix_acl: Clear SGID bit when setting file permissions"
    This reverts commit 6b0c893dc08060d6999b07136391d8a298678dae.
    
    Revert "fbcmap: prevent memory overflow"
    This reverts commit 0db8b9db0e63e905d769221258c0f2dce79b01bf.
    
    Revert "drivers: video: Add bounds checking in fb_cmap_to_user"
    This reverts commit 58df98cc2042346cc851b89c1351d7b758fd5b23.
    
    Revert "mm/huge_memory.c: respect FOLL_FORCE/FOLL_COW for thp"
    This reverts commit a93e7698ed6ee544d28de77b93b1647098e5f54d.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/video/fbcmap.c
+++ drivers/video/fbcmap.c
@@ -196,20 +196,18 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	int tooff = 0, fromoff = 0;
 	int size;
 
-	if (!to || !from || (int)(to->start) < 0)
+	if (!to || !from)
 		return -EINVAL;
 
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
 		tooff = from->start - to->start;
-	if ((to->len <= tooff) || (from->len <= fromoff))
-		return -EINVAL;
-
 	size = to->len - tooff;
-
 	if (size > (int) (from->len - fromoff))
 		size = from->len - fromoff;
+	if (size <= 0)
+		return -EINVAL;
 	size *= sizeof(u16);
 
 	if (from->red && to->red)
